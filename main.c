a717f7b1fc6bd99a1150a4ca814b60d759bd6c7c
Clement Callejon <clement.callejon@tutanota.com>
2024-08-27 02:05:52 +0200

    Added terminal control functionnalities to clear console and display game in separate env

--- main.c
+++ main.c
@@ -2,36 +2,48 @@
 #include <stdio.h>
 
 /**
-* main - Entry point for tiC-taC-toe.
-*
-* Return: 0 for success.
-*/
+ * main - Entry point for tiC-taC-toe.
+ *
+ * Return: 0 for success.
+ */
 int main(void)
 {
 	char board[10] = "         ";
 	char winner = ' ', curr_player = 'X';
 	unsigned int chosen_slot = 0, turn_count = 0;
 
+	enter_alt_screen();
+
+	print_board(board);
 	while (winner == ' ')
 	{
+		clear_screen();
 		++turn_count;
 		print_board(board);
+
+		/* Prompt player to play its move */
 		printf("Choose your slot :");
 		scanf("%u", &chosen_slot);
 
-		place_chip(chosen_slot, curr_player, board);
+		while(place_chip(chosen_slot, curr_player, board) == -2)
+		{
+			printf("This slot is already taken, choose again :");
+			scanf("%u", &chosen_slot);
+
+		}
+
 
 		if (turn_count >= 3 && check_win(curr_player, board) == 1)
 		{
-			print_board(board);
 			winner = curr_player;
 			printf("%c player wins !\n", winner);
+			exit_alt_screen();
 			break;
 		}
 
 		curr_player = switch_player(curr_player);
 
-		printf("------------------");
+		printf("------------------\n");
 	}
 	return (0);
 }
--- main.h
+++ main.h
@@ -11,4 +11,8 @@ int place_chip(unsigned int pos, char chip_symbol, char *board);
 char switch_player(char curr_player);
 int ask_player_move();
 
+#define clear_screen() puts("\x1B[2J")
+#define enter_alt_screen() puts("\033[?1049h\033[H")
+#define exit_alt_screen() puts("\033[?1049l")
+
 #endif
